4d6ee823a83f5fe92c911563a41d073fe25c3758
OmarGomaaFCi <omargomaafcitu@gmail.com>
2024-04-05 22:18:02 +0200

    Add initial dependencies

---
+++ src/main/java/com/example/maidsquizapi/MaidsQuizApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.maidsquizapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaidsQuizApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaidsQuizApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/maidsquizapi/MaidsQuizApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maidsquizapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaidsQuizApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
